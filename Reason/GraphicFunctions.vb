9fae107cf937544e975b678c5cbeef0afb5a9e02
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-07-06 08:28:37 +0500

    added graphic functions module

---
+++ Reason/GraphicFunctions.vb
@@ -0,0 +1,10 @@
+﻿Module GraphicFunctions
+
+
+    Sub DrawLine(g As Graphics, x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        g.DrawLine(Pens.Black, x1, y1, x2, y2)
+    End Sub
+
+
+
+End Module
